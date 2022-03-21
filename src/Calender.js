5c374385f06b6ed9a9a10fbaae6a97ad1bd88d9c
Montensia Banks <montensia@gmail.com>
2022-03-20 22:01:55 -0500

    Added the Calendar component

---
+++ src/Calender.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Calendar extends React.Component {
+  render() {
+    return (
+      <>
+        <section className="container">
+          <div className="calendar"></div>
+        </section>
+      </>
+    );
+  }
+}

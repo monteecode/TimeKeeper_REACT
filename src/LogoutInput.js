5ae196a450c56f57e3afc9de383078f2720bcad4
Montensia Banks <montensia@gmail.com>
2022-04-06 12:22:21 -0500

    Added style to welcome section

--- src/LogoutInput.js
+++ src/LogoutInput.js
@@ -5,11 +5,20 @@ import { logoutUser } from "./actions/login";
 
 const LogoutInput = (props) => {
   return (
-    <div id="welcomeUser">
-      {/* the && is like  aternary operator but just for two things. If the first is true, then do the second. Otherwise do nothing*/}
-      <p>Welcome, {props.user && props.user.name}</p>
-      <button onClick={() => props.logoutUser(props.history)}>logout</button>
-    </div>
+    <section id="welcomeUser" className="container">
+      <p>
+        Welcome,{" "}
+        {props.user &&
+          props.user.name[0].toUpperCase() + props.user.name.slice(1) + "!"}
+      </p>
+      {/* If user exists then display user name */}
+      <button
+        className="btn btn-sm btn-danger"
+        onClick={() => props.logoutUser(props.history)}
+      >
+        logout
+      </button>
+    </section>
   );
 };
 

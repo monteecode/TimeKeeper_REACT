8bd0b3ec0340d74f091e14a8fe9b58ad46e45f77
Montensia Banks <montensia@gmail.com>
2022-04-08 12:53:20 -0500

    Changed base url to heroku url

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,4 +1,5 @@
-const BASEURL = "http://localhost:4000";
+//const BASEURL = "http://localhost:4000";
+const BASEURL = "https://timekeeper-rails.herokuapp.com/";
 //const USER_URL = "http://localhost:3000/users";
 
 export default BASEURL;
